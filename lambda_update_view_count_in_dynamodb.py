09e40a7b9364a3e8bc55ecac8f6a9442d226b434
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-12-27 16:35:07 -0500

    Update function to push test value into table

--- lambda_update_view_count_in_dynamodb.py
+++ lambda_update_view_count_in_dynamodb.py
@@ -1,1 +1,28 @@
 # Lambda function to read current website view count from DynamoDB, increment count, and resave to DynamoDB table
+
+import boto3
+import json
+
+dynamodb_resource = boto3.resource('dynamodb')
+
+def lambda_handler(event, context):
+    
+    table_name = 'view-counter-cloud-resume-challenge'
+    table = dynamodb_resource.Table(table_name)
+
+    count_item = event    
+    
+    try:
+        table.put_item(
+                Item=count_item
+            )
+    except Exception as e:
+        print(f'Error:  not able to add item {count_item} to DyanmoDB table {table_name}.')
+    
+    return None
+
+    # SAMPLE VALUE CONFIGURED IN TEST EVENT
+    # count_item = {
+    #     'count_id': '0',
+    #     'view_count': 1
+    # }
