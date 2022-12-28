62c41814cc05aff5ca79a9698a111140f4d36ccd
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-12-27 21:16:30 -0500

    Update to read from & write to DynamoDB table

--- lambda_update_view_count_in_dynamodb.py
+++ lambda_update_view_count_in_dynamodb.py
@@ -3,26 +3,28 @@
 import boto3
 import json
 
-dynamodb_resource = boto3.resource('dynamodb')
-
-def lambda_handler(event, context):
+dynamodb = boto3.resource('dynamodb')
     
-    table_name = 'view-counter-cloud-resume-challenge'
-    table = dynamodb_resource.Table(table_name)
+table_name = 'view-counter-cloud-resume-challenge'
+table = dynamodb.Table(table_name)
 
-    count_item = event    
-    
-    try:
-        table.put_item(
-                Item=count_item
-            )
-    except Exception as e:
-        print(f'Error:  not able to add item {count_item} to DyanmoDB table {table_name}.')
-    
-    return None
+# get current view count value from DB
+resp = table.get_item(
+    Key={
+        'count_id': 'total_views'
+    }
+)
 
-    # SAMPLE VALUE CONFIGURED IN TEST EVENT
-    # count_item = {
-    #     'count_id': '0',
-    #     'view_count': 1
-    # }
+# increment count value up for current view
+previous_count = resp['Item']['current_count']
+new_count = previous_count + 1
+
+# update DB with new view count value
+table.put_item(
+    Item={
+        'count_id': 'total_views',
+        'current_count': new_count
+    }
+)
+
+return new_count
