8a0ebc3e05ea2824d0311997953525acb15103e9
William Lewis <31929244+williamlewis@users.noreply.github.com>
2023-01-09 11:32:00 -0500

    Delete page_view_counter.js
    File relocated to Front-End directory

--- 02 - Website Back-End/page_view_counter.js
+++
@@ -1,8 +0,0 @@
-const api_url = 'https://gqzq4elpqa.execute-api.us-east-1.amazonaws.com/Production/count'
-
-    fetch(api_url, {
-        method: 'POST'
-    })
-        .then((response) => response.json())
-        .then((data) => {document.getElementById('displayed-count').innerHTML = 'Total Page Views:  ' + data['total_views']});
-                    
