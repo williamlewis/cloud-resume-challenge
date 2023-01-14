adce3e27156c9bfc1d126b9527d841c220408ff0
William Lewis <31929244+williamlewis@users.noreply.github.com>
2023-01-13 23:56:42 -0500

    Reference API source_arn in lambda permission

--- 03 - Website Infra/main.tf
+++ 03 - Website Infra/main.tf
@@ -20,9 +20,7 @@ provider "archive" {}
 
 
 data "archive_file" "zip" {
-  type = "zip"
-  # source_file = "lambda_update_view_count_in_dynamodb.py"
-  # output_path = "update_view_count.zip"
+  type        = "zip"
   source_file = "${path.module}./02 - Website Back-End/lambda_update_view_count_in_dynamodb.py"
   output_path = "${path.module}./02 - Website Back-End/update_view_count.zip"
 }
@@ -112,7 +110,7 @@ EOF
 resource "aws_lambda_function" "lambda-view-counter-function" {
   function_name = "lambda-view-counter-function"
 
-  filename         = data.archive_file.zip.output_path #"update_view_count.zip"
+  filename         = data.archive_file.zip.output_path # "update_view_count.zip"
   source_code_hash = data.archive_file.zip.output_base64sha256
 
   role    = aws_iam_role.iam_lambda_role.arn
@@ -127,141 +125,98 @@ resource "aws_lambda_function" "lambda-view-counter-function" {
 }
 
 
-# # -----------------------------------------
-# # ////////       API Gateway       ////////
-# # -----------------------------------------
+# -----------------------------------------
+# ////////       API Gateway       ////////
+# -----------------------------------------
 
-# # REST API
-# resource "aws_api_gateway_rest_api" "api-to-lambda-view-count" {
-#   name        = "api-to-lambda-view-count"
-#   description = "Gateway -> Lambda -> DynamoDB"
+# REST API
+resource "aws_api_gateway_rest_api" "api-to-lambda-view-count" {
+  name        = "api-to-lambda-view-count"
+  description = "Gateway -> Lambda -> DynamoDB"
 
-#   endpoint_configuration {
-#     types = ["REGIONAL"]
-#   }
-# }
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
 
-# # API Resource (path end for URL)
-# resource "aws_api_gateway_resource" "api-resource" {
-#   parent_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.root_resource_id
-#   path_part   = "count"
-#   rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-# }
+# API Resource (path end for URL)
+resource "aws_api_gateway_resource" "api-resource" {
+  parent_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.root_resource_id
+  path_part   = "count"
+  rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
+}
 
-# # Request Method
-# resource "aws_api_gateway_method" "api-post-method" {
-#   authorization = "NONE"
-#   http_method   = "POST"
-#   resource_id   = aws_api_gateway_resource.api-resource.id
-#   rest_api_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-# }
+# Request Method
+resource "aws_api_gateway_method" "api-post-method" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.api-resource.id
+  rest_api_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.id
+}
 
-# # Integration (link to Lambda function)
-# resource "aws_api_gateway_integration" "api-lambda-integration" {
-#   rest_api_id             = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   resource_id             = aws_api_gateway_resource.api-resource.id
-#   http_method             = aws_api_gateway_method.api-post-method.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.lambda-view-counter-function.invoke_arn
-# }
+# Integration (link to Lambda function)
+resource "aws_api_gateway_integration" "api-lambda-integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api-to-lambda-view-count.id
+  resource_id             = aws_api_gateway_resource.api-resource.id
+  http_method             = aws_api_gateway_method.api-post-method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda-view-counter-function.invoke_arn
+}
 
-# # Deployment (to stage for use)
-# resource "aws_api_gateway_deployment" "api-deployment" {
-#   rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
+# Deployment (to stage for use)
+resource "aws_api_gateway_deployment" "api-deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
 
-#   triggers = {
-#     # NOTE: The configuration below will satisfy ordering considerations,
-#     #       but not pick up all future REST API changes. More advanced patterns
-#     #       are possible, such as using the filesha1() function against the
-#     #       Terraform configuration file(s) or removing the .id references to
-#     #       calculate a hash against whole resources. Be aware that using whole
-#     #       resources will show a difference after the initial implementation.
-#     #       It will stabilize to only change when resources change afterwards.
-#     redeployment = sha1(jsonencode([
-#       aws_api_gateway_resource.api-resource.id,
-#       aws_api_gateway_method.api-post-method.id,
-#       aws_api_gateway_integration.api-lambda-integration.id,
-#     ]))
-#   }
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.api-resource.id,
+      aws_api_gateway_method.api-post-method.id,
+      aws_api_gateway_integration.api-lambda-integration.id,
+    ]))
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# # Stage
-# resource "aws_api_gateway_stage" "api-stage" {
-#   deployment_id = aws_api_gateway_deployment.api-deployment.id
-#   rest_api_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   stage_name    = "prod"
-# }
+# Stage
+resource "aws_api_gateway_stage" "api-stage" {
+  deployment_id = aws_api_gateway_deployment.api-deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api-to-lambda-view-count.id
+  stage_name    = "prod"
+}
 
-# # Permission (from Lambda to API)
-# resource "aws_lambda_permission" "lambda-permission-to-api" {
-#   statement_id  = "AllowAPIGatewayInvoke"
-#   action        = "lambda:InvokeFunction"
-#   function_name = "lambda-view-counter-function"
-#   principal     = "apigateway.amazonaws.com"
+# Permission (from Lambda to API)
+resource "aws_lambda_permission" "lambda-permission-to-api" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "lambda-view-counter-function"
+  principal     = "apigateway.amazonaws.com"
 
-#   # The /*/*/* part allows invocation from any stage, method and resource path
-#   # within API Gateway REST API.
-#   source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/*/*/*"
-#   # source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/prod/POST/count"
-#   # source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/${aws_api_gateway_stage.api-stage.stage_name}/${aws_api_gateway_method.api-post-method.http_method}/${aws_api_gateway_resource.api-resource.path_part}"
-# }
+  # source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/*/*/*" # the /*/*/* part allows invocation from any stage, method and resource path within API Gateway REST API.
+  # source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/prod/POST/count" # hard-coded values work
+  source_arn = "${aws_api_gateway_rest_api.api-to-lambda-view-count.execution_arn}/${aws_api_gateway_stage.api-stage.stage_name}/${aws_api_gateway_method.api-post-method.http_method}/${aws_api_gateway_resource.api-resource.path_part}"
+}
 
 
-# # # --------------------------------------------------------------
-# # # ////////       Enable CORS (API Gateway Cont'd)       ////////
-# # # --------------------------------------------------------------
+# # --------------------------------------------------------------
+# # ////////       Enable CORS (API Gateway Cont'd)       ////////
+# # --------------------------------------------------------------
 
-# # CORS-Enabling Method
-# resource "aws_api_gateway_method" "api-cors-method" {
-#   rest_api_id      = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   resource_id      = aws_api_gateway_resource.api-resource.id
-#   http_method      = "OPTIONS"
-#   authorization    = "NONE"
-#   api_key_required = false
-# }
 
-# # CORS-Enabling Method Response
-# resource "aws_api_gateway_method_response" "api-cors-method-response" {
-#   rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   resource_id = aws_api_gateway_resource.api-resource.id
-#   http_method = aws_api_gateway_method.api-cors-method.http_method
-#   status_code = "200"
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = true
-#     "method.response.header.Access-Control-Allow-Methods" = true
-#     "method.response.header.Access-Control-Allow-Origin"  = true
-#   }
-# }
+module "api-gateway-enable-cors" {
+  source  = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
 
-# # CORS-Enabling Integration
-# resource "aws_api_gateway_integration" "api-cors-integration" {
-#   rest_api_id          = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   resource_id          = aws_api_gateway_resource.api-resource.id
-#   http_method          = "OPTIONS"
-#   type                 = "MOCK"
-#   passthrough_behavior = "WHEN_NO_MATCH"
-#   request_templates = {
-#     "application/json" : "{\"statusCode\": 200}"
-#   }
-# }
-
-# # CORS-Enabling Integration Response
-# resource "aws_api_gateway_integration_response" "options" {
-#   rest_api_id = aws_api_gateway_rest_api.api-to-lambda-view-count.id
-#   resource_id = aws_api_gateway_resource.api-resource.id
-#   http_method = aws_api_gateway_integration.api-cors-integration.http_method
-#   status_code = "200"
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-#     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-#     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-#   }
-# }
+  api_id          = aws_api_gateway_rest_api.api-to-lambda-view-count.id # "<api_id>"
+  api_resource_id = aws_api_gateway_resource.api-resource.id             # "<api_resource_id>"
+}
