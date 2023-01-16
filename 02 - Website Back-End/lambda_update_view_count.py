7d92eb00dbd9629f875064b26b2a96aff6f52fe9
William Lewis <31929244+williamlewis@users.noreply.github.com>
2023-01-15 23:43:40 -0500

    Test update for GitHub Actions event

--- 02 - Website Back-End/lambda_update_view_count.py
+++ 02 - Website Back-End/lambda_update_view_count.py
@@ -7,25 +7,25 @@ import os
 
 dynamodb = boto3.resource('dynamodb')
 
-# table name is stored as an environmental variable to differentiate during production vs. testing
+# Table name is stored as an environmental variable to differentiate during production vs. testing
 table_name = os.environ['TABLE_NAME']
 
 def lambda_handler(event, context):
 
     table = dynamodb.Table(table_name)
 
-    # get current view count value from DB
+    # Get current view count value from DB
     resp = table.get_item(
         Key={
             'count_id': 'total_views'
         }
     )
 
-    # increment count value up for current view
+    # Increment count value up for current view
     previous_count = resp['Item']['current_count']
     new_count = previous_count + 1
 
-    # update DB with new view count value
+    # Update DB with new view count value
     table.put_item(
         Item={
             'count_id': 'total_views',
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
         }
     )
 
-    # build JSON response to return to API
+    # Build JSON response to return to API
     api_response = {
         'statusCode': 200,
         'headers': {
