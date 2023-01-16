d14217ad3094d286fe911ec09df722a7477b7e2f
William Lewis <31929244+williamlewis@users.noreply.github.com>
2023-01-15 23:46:38 -0500

    Update to test GH Actions (1-9)

--- 02 - Website Back-End/lambda_update_view_count.py
+++ 02 - Website Back-End/lambda_update_view_count.py
@@ -1,6 +1,7 @@
 # Lambda function invoked by POST request to API Gateway URL endpoint via Javascript script
 # Reads current website view count from DynamoDB, increments count, then updates value in DynamoDB table before returning JSON response to API Gateway
 
+# Test comment9 for GitHub Actions
 import boto3
 import json
 import os
